b3ab5abf9afd2f606c1431c80e6fb93edf578f79
Lucas Ringer <lucas.ringer2006@gmail.com>
2025-10-16 09:34:32 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hallo Welt, meine App läuft!</h1>
+        <p>
+          Diese Seite wurde erfolgreich auf Heroku bereitgestellt.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
