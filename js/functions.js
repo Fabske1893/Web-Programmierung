8c951ac8fb965cce603ca1d7d0bbf6c6b0f1ef57
SuperMan <supernameatdeinemum.com>
2025-10-19 12:00:35 +0200

    Korrektur Ordnerstruktur

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+function createRecipe(){
+
+    
+    setTimeout(() => {
+        alert('Das Rezept wurde erfolgreich erstellt!');
+        window.location.href = '../Homepage.html'; // Zur Homepage weiterleiten
+    }, 500); // 500ms Verzögerung, um die Animation sichtbar zu machen
+}
+
+
+
+function goToAccount(){
+    window.location.href = './login/Account.html'; // Zur Account-Seite weiterleiten
+}
+
+
+function likeRecipe(){}
+
+function deleteRecipe(){
+    
+    alert('Alle Rezepte wurden gelöscht!');
+    window.location.href= '../Homepage.html'; // Zur Homepage weiterleiten
+    
+}
